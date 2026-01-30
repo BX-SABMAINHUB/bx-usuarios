cc1fdee1d8be72ab06d22aa2a2f8fc96fb247b54
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-30 19:59:04 +0100

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -1,20 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
-export default function Redirect() {
+export default function Page() {
   const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    if (id) {
-      fetch(`/api/links?id=${id}`)
-        .then(res => res.json())
-        .then(data => {
-          if (data && data.url) window.location.href = data.url;
-        })
-        .catch(err => console.error(err));
-    }
-  }, [id]);
-
   return <div>Redireccionando...</div>;
 }
