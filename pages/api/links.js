48d4ca978fbc2cf72c7191505c3c8cfbfe4c60ac
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-30 20:17:21 +0100

    Update links.js

--- pages/api/links.js
+++ pages/api/links.js
@@ -2,21 +2,19 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db("bx_database"); // Así se llamará tu base de datos
+  const db = client.db("bx_database");
 
   if (req.method === "GET") {
     const { id } = req.query;
     const link = await db.collection("links").findOne({ id: id });
     if (link) {
       return res.status(200).json(link);
-    } else {
-      return res.status(404).json({ error: "Link no encontrado" });
     }
+    return res.status(404).json({ error: "No encontrado" });
   }
-  
-  // Si tu Dashboard envía datos, este código los guarda
+
   if (req.method === "POST") {
-    const data = req.body;
+    const data = req.body; // Aquí recibe lo que envías desde el Dashboard
     await db.collection("links").insertOne(data);
     return res.status(200).json({ success: true });
   }
