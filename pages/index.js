2dd8eb0ce0c4efa858e3d9062e60254982b90eb6
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-31 14:02:31 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 /**
